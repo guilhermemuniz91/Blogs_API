49647ebc5e23b33a82d71af396d86d91cffc2ff8
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-10-31 14:28:26 -0300

    req02 feito

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const User = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    }
+    );
+
+    User.associate = (models) => {
+      User.hasMany(models.BlogPost, { foreignKey: 'id', as: 'BlogPost' });
+    };
+
+    return User;
+  };
+  
+  module.exports = User;
