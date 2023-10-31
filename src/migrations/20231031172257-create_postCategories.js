781a98ed0e0e466a4fd64ee87fda3599680356d3
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-10-31 14:24:57 -0300

    req01 feito

---
+++ src/migrations/20231031172257-create_postCategories.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        type: Sequelize.INTEGER
+      },
+
+      category_id: {
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        type: Sequelize.INTEGER
+      },
+
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
